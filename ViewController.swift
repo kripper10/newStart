3a6b333e053cabcb0ca3fbd2ad528911af1563ae
Kripper <kripper10@gmail.com>
2016-09-21 22:44:11 +0100

    First Commit
    Lets see

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  newStart
+//
+//  Created by Chris on 21/09/2016.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test");
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
